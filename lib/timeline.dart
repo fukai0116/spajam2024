6a315993ad88efae6f71fb28ec013f1c045298f0
deforu <22j1162@andrew.ac.jp>
2024-08-24 22:12:33 +0900

    timelineページの追加

---
+++ lib/timeline.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TimelinePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Timeline'),
+      ),
+      body: Center(
+        child: Text('Timeline Page Content'),
+      ),
+    );
+  }
+}
